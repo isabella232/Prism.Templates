057c72c2c70a2ce3a462bde450d8d3873e26fc91
Dan Siegel <me@dansiegel.net>
2020-08-05 16:31:11 -0700

    remove default ctor for Xamarin Previewer

--- Xamarin.Forms/BlankApp/BlankApp/App.xaml.cs
+++ Xamarin.Forms/BlankApp/BlankApp/App.xaml.cs
@@ -12,14 +12,10 @@ namespace BlankApp
 {
     public partial class App
     {
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
-        public App() : this(null) { }
-
-        public App(IPlatformInitializer initializer) : base(initializer) { }
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
 
         protected override async void OnInitialized()
         {
